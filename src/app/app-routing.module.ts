73868a26497bae0388f1458a6bb9cfb8c70d063d
John1316 <johnmaher179@gmail.com>
2023-07-07 21:28:23 +0300

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsComponent } from './pages/posts/posts.component';
+import { AboutComponent } from './pages/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/posts' , pathMatch:'full'},
+  {path: 'posts' , component: PostsComponent },
+  {path: 'about' , component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { PostsComponent } from './pages/posts/posts.component';
+import { HeaderComponent } from './shared-pages/header/header.component';
+import { AsideComponent } from './shared-pages/aside/aside.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    HeaderComponent,
+    AsideComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
